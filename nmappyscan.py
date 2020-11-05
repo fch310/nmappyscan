2f15fa8d367d134805e2fea9974405ad7e4f6550
fch310 <fch310@DESKTOP-L7SVH1P.localdomain>
2020-11-04 22:44:33 -0600

    el usuario puede introducir el nombre del archivo que desea leer

--- nmappyscan.py
+++ nmappyscan.py
@@ -1,8 +1,10 @@
 #Automate script for scan IP with nmap
 
-file = open ("sample.txt","r")
+value = input('Enter the file name: ')
+file = open (value,"r")
 
 content = file.readlines()
 for line in content:
     IP = line.split()
     print(IP)
+
